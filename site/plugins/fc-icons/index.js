9e2d279380e1bb6d481d8393bee36982db3d4232
Max Esnée <contact@max-esnee.com>
2025-10-07 12:01:50 +0200

    Add custom icons for rounded corners on image + add more options

---
+++ site/plugins/fc-icons/index.js
@@ -0,0 +1,12 @@
+panel.plugin('maxesnee/fc-icons', {
+   icons: {
+      'corner-radius-10':  '<rect x="1" y="1" width="46" height="46" rx="3"  fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-20':  '<rect x="1" y="1" width="46" height="46" rx="5"  fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-30':  '<rect x="1" y="1" width="46" height="46" rx="7"  fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-40':  '<rect x="1" y="1" width="46" height="46" rx="9"  fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-60':  '<rect x="1" y="1" width="46" height="46" rx="13" fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-80':  '<rect x="1" y="1" width="46" height="46" rx="17" fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-100': '<rect x="1" y="1" width="46" height="46" rx="21" fill="none" stroke-width="2" stroke="currentColor" />',
+      'corner-radius-120': '<rect x="1" y="1" width="46" height="46" rx="25" fill="none" stroke-width="2" stroke="currentColor" />'
+   }
+});
--- site/plugins/fc-icons/index.php
+++ site/plugins/fc-icons/index.php
@@ -11,11 +11,14 @@ function getIcon($name) {
 }
 
 Kirby::plugin('maxesnee/fc-icons', [
+    // Add kirby-tags for adding icons in text fields
     'tags' => [
         'icon' => [
             'html' => function($tag) {
                 return file_get_contents(getIcon($tag->value));
             }
         ]
-    ]
+    ],
+    // Add custom icons for the panel
+    'icons' => []
 ]);
