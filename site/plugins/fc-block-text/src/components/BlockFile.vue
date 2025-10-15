a7748b48594a0736c2c609b83fd7fd2aa2b31d3e
Max Esnée <contact@max-esnee.com>
2025-10-15 15:12:22 +0200

    Small fix in BlockFile.vue template

--- site/plugins/fc-block-text/src/components/BlockFile.vue
+++ site/plugins/fc-block-text/src/components/BlockFile.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="k-block k-block-type-link k-block-type-default" @click="open" @update="update">
-      <k-block-title :fieldset="{ icon: 'download', name: this.content.label, label: this.content.label == '' ? 'Lien' : ''}"/>
+      <k-block-title :fieldset="{ icon: 'download', name: content.label, label: content.label == '' ? 'Lien' : ''}"/>
       <div class="fc-blocks-controls" @click="open">
          <k-color-frame :color="content.color" ratio="1/1"/>
       </div>
