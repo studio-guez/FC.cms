fed4375bd0cbfad568e2d2de747b2d37849c8e8d
Max Esnée <max.esnee@gmail.com>
2025-11-07 10:54:51 +0100

    Add horizontal rule block

---
+++ site/plugins/fc-blocks/src/components/BlockHorizontalRule.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="k-block k-block-type-hr k-block-type-default" @click="open" @update="update">
+      <hr />
+      <div class="fc-blocks-controls" @click="open">
+         <k-color-frame :color="content.color" ratio="1/1"/>
+      </div>
+   </div>
+</template>
+
+<style scoped lang="css">
+
+   hr {
+      height: 0.1em;
+      background-color: var(--color-text);
+      margin: 0.55rem 3rem 0.55rem 0;
+   }
+   .fc-blocks-controls {
+      position: absolute;
+      top: 0.5rem;
+      right: 0.75rem;
+      cursor: pointer;
+   }
+
+   .k-color-frame {
+      width: 1.65rem;
+      height: 1.65rem;
+   }
+</style>
--- site/plugins/fc-blocks/src/index.js
+++ site/plugins/fc-blocks/src/index.js
@@ -2,6 +2,7 @@ import BlockText from "./components/BlockText.vue";
 import BlockImage from "./components/BlockImage.vue";
 import BlockLink from "./components/BlockLink.vue";
 import BlockFile from "./components/BlockFile.vue";
+import BlockHorizontalRule from "./components/BlockHorizontalRule.vue";
 import BlockNewsletter from "./components/BlockNewsletter.vue";
 
 panel.plugin('maxesnee/fc-blocks', {
@@ -10,6 +11,7 @@ panel.plugin('maxesnee/fc-blocks', {
       image: BlockImage,
       link: BlockLink,
       file: BlockFile,
+      hr: BlockHorizontalRule,
       newsletter: BlockNewsletter
    }
 });
