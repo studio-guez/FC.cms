9a2858bc651c96f1793876eec5f2e52e328bfa13
Max Esnée <max.esnee@gmail.com>
2025-12-12 14:07:53 +0100

    Add field method to format text, and convert to relative urls

--- site/plugins/fc-blocks/index.php
+++ site/plugins/fc-blocks/index.php
@@ -1,5 +1,21 @@
 <?php
 
 use Kirby\Cms\App as Kirby;
+use Kirby\Text\KirbyTags;
+use Kirby\Toolkit\Str;
 
-Kirby::plugin('maxesnee/fc-blocks', []);
+Kirby::plugin('maxesnee/fc-blocks', [
+   'fieldMethods' => [
+        'formatText' => function($field) {
+            $field->value = KirbyTags::parse($field->value);
+            $field->value = smartypants($field->value);
+            return $field;
+        },
+        'absoluteToRelativeUrls' => function($field) {
+            $rootUrl = kirby()->url('index');
+            $field->value = str_replace($rootUrl, '', (string)$field->value);
+
+            return $field;
+        }
+    ]
+]);
