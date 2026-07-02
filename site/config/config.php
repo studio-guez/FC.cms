70c83933b6c343e9cb1c77f3a90022417dd21ae8
Max Esnée <max.esnee@gmail.com>
2026-07-02 14:37:36 +0200

    Fix frontend url in config

--- site/config/config.php
+++ site/config/config.php
@@ -6,7 +6,7 @@ use Kirby\Toolkit\Str;
 header("Access-Control-Allow-Origin: *");
 
 return [
-    'frontendUrl' => 'https://lesfantastiquescommuns.ch',
+    'frontendUrl' => 'https://lesfantastiquescommuns.cc',
     'api' => [
         'basicAuth' => true,
         'allowInsecure' => true,
